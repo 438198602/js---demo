23f52ea40439e66c9da7ab1c956c6a6bdebcac93
438198602 <438198602@qq.com>
2021-09-08 15:29:10 +0800

    20210908

--- nodejs/image-diff/puppeteer.js
+++ nodejs/image-diff/puppeteer.js
@@ -36,63 +36,95 @@ function writeFile(path, data) {
 }
 
 (async () => {
-    const browser = await puppeteer.launch({
-        headless: true,
-        executablePath: 'C:/Users/ycl/Downloads/chrome-win/chrome.exe',
-        defaultViewport: {
-            width: 1920,
-            height: 1080
-        }
-    });
-    const page = await browser.newPage();
-
-    let filePath = 'dist';
-    // await delDir(filePath);
-    await addDir(filePath);
-
-    // await book118();
-    await search();
-
-    await page.screenshot({
-        path: filePath + '/screenshot.png'
-    });
-
-    await browser.close();
-
-    async function book118() {
-        await page.goto('https://max.book118.com/');
-        await page.waitForTimeout(300);
-
-        // 模拟登录
-        await page.click('#header ul.menu .member a');
-        await page.waitForTimeout(1000);
-        const loginIframe = await page.frames().find(frame => {
-            return frame.name() == 'layui-layer-iframe1';
+    try {
+        const browser = await puppeteer.launch({
+            headless: true,
+            executablePath: 'C:/Users/ycl/Downloads/chrome-win/chrome.exe',
+            defaultViewport: {
+                width: 1920,
+                height: 1080
+            }
         });
-        await loginIframe.click('.login-box .quick-link a.login-account');
-        await loginIframe.waitForTimeout(300);
-        await loginIframe.type('.account-form input.username', 'fuyang');
-        await loginIframe.type('.account-form input[type="password"]', '123456');
-        await loginIframe.waitForTimeout(300);
-        await loginIframe.click('.account-form .button-box input.submit');
-        await loginIframe.waitForTimeout(500);
-    }
+        const page = await browser.newPage();
+    
+        let filePath = 'dist';
+        // await delDir(filePath);
+        await addDir(filePath);
+    
+        // await book118();
+        await search();
+    
+        await page.screenshot({
+            path: filePath + '/screenshot.png'
+        });
 
-    async function search() {
-        let args = process.argv.splice(2);
-        if (!args.length) {
-            console.log('请输入参数！格式为：nodejs puppeteer.js [关键字]');
-            return;
+        await browser.close();
+        console.log('---end---');
+
+        async function book118() {
+            await page.goto('https://max.book118.com/');
+            await page.waitForTimeout(300);
+
+            // 模拟登录
+            await page.click('#header ul.menu .member a');
+            await page.waitForTimeout(1000);
+            const loginIframe = await page.frames().find(frame => {
+                return frame.name() == 'layui-layer-iframe1';
+            });
+            await loginIframe.click('.login-box .quick-link a.login-account');
+            await loginIframe.waitForTimeout(300);
+            await loginIframe.type('.account-form input.username', 'fuyang');
+            await loginIframe.type('.account-form input[type="password"]', '123456');
+            await loginIframe.waitForTimeout(300);
+            await loginIframe.click('.account-form .button-box input.submit');
+            await loginIframe.waitForTimeout(500);
         }
 
-        await page.goto('https://www.baidu.com/s?wd=' + args[0]);
-        await page.emulate(puppeteer.devices['iPhone 6']);
-        await page.waitForTimeout(300);
+        async function search(searchUrl = '') {
+            let baseUrl = 'https://www.baidu.com/s?wd=',
+                url = '',
+                keyword = '',
+                args = process.argv.splice(2);
+            keyword = '微任务 宏任务';
+            if (searchUrl) {
+                url = searchUrl;
+            } else {
+                if (keyword) {
+                    url = baseUrl + keyword;
+                } else {
+                    if (!args.length) {
+                        console.log('\x1B[31m%s\x1B[0m', '请输入参数！格式为：nodejs puppeteer.js [关键字]');
+                        return;
+                    } else {
+                        url = baseUrl + args[0];
+                    }
+                }
+            }
+            url = 'https://www.zhihu.com/';
 
-        const html = await page.evaluate(() => {
-            // return document.getElementsByTagName('html')[0].outerHTML;
-            return document.body.innerHTML;
-        });
-        writeFile(filePath + '/search.html', html);
+            await page.goto(url);
+            await page.emulate(puppeteer.devices['iPhone 6']);
+            await page.waitForTimeout(300);
+
+            let html = await page.evaluate(async () => {
+                try {
+                    let $ = window.$,
+                        rootHtml = $ ? $('body').html() : document.body.innerHTML,
+                        scriptReg = /<script.*?>([\s\S]+?)<\/script>/img,
+                        noscriptReg = /<noscript.*?>([\s\S]+?)<\/noscript>/img,
+                        styleReg = /<style.*?>([\s\S]+?)<\/style>/img,
+                        noteReg = /<!--.*?>([\s\S]+?)-->/img,
+                        nbspReg = /&nbsp;/img;
+                    rootHtml = rootHtml.replace(scriptReg, '').replace(noscriptReg, '').replace(styleReg, '').replace(noteReg, '').replace(nbspReg, '');
+
+                    return rootHtml;
+                } catch (error) {
+                    return 'evaluate error => ' + error;
+                }
+            });
+            writeFile(filePath + '/search.html', html);
+        }
+    } catch (error) {
+        console.log(error);
     }
 })();
